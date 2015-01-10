dcdf8a3fe19fb1b397304290ee6f72d6d67b321c
flip <ansuz@transitiontech.ca>
2015-01-10 15:06:20 -0500

    exportable as a lib

--- php.js
+++ php.js
@@ -1,29 +1,41 @@
 var http=require("http");
 var fs=require("fs");
+var php={};
 
-var readAndDo=function(path,f){
+var readAndDo=php.readAndDo=function(path,f){
   fs.readFile(path,'utf8',function(err,data){
     if(err)console.log(err);
     else f(data);
   });
+  return '';
 };
 
-var route=function(req,res){
-  readAndDo('index.phpjs',function(erm){
-    var C=0;
-    var O={};
-    res.end(erm.replace(/<\?js(.|\s)*?\?>/mg,function(x){
-      var R="";
-      var echo=function(x){
-        R+=(x+'\n');
-        console.log(x);
-        return x;
-      };
-      eval(x.slice(4,-3));
-      return R;  
-    }));
-  });
-};  
+var main=php.main=function(opt){
+  opt=opt||{};
+  var path=opt.path||"";
+  var def=opt.def||'index.phpjs';
 
-var server=http.createServer(route);
-server.listen(8080);
+  return function(req,res){
+    console.log(readAndDo(path+(req.url.slice(1)||def),function(erm){
+      var C=0;
+      var O={};
+      res.end(erm.replace(/<\?js(.|\s)*?\?>/mg,function(x){
+        var R="";
+        var echo=function(x){
+          R+=(x+'\n');
+          console.log(x);
+          return x;
+        };
+        eval(x.slice(4,-3));
+        return R;  
+      }));
+    }));
+  };
+};
+
+if(!module.parent){
+  var server=http.createServer(main({path:'views/'}));
+  server.listen(8080);
+}else{
+  module.exports=php;
+}
