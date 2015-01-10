9ab08451eb034c8b4e3086416646787a996e1314
flip <ansuz@transitiontech.ca>
2015-01-10 15:41:04 -0500

    works as an unmon plugin

--- php.js
+++ php.js
@@ -14,22 +14,32 @@ var main=php.main=function(opt){
   opt=opt||{};
   var path=opt.path||"";
   var def=opt.def||'index.phpjs';
+  var F404=function(req,res){
+    res.end('404');
+  };
 
-  return function(req,res){
-    console.log(readAndDo(path+(req.url.slice(1)||def),function(erm){
-      var C=0;
-      var O={};
-      res.end(erm.replace(/<\?js(.|\s)*?\?>/mg,function(x){
-        var R="";
-        var echo=function(x){
-          R+=(x+'\n');
-          console.log(x);
-          return x;
-        };
-        eval(x.slice(4,-3));
-        return R;  
-      }));
-    }));
+  return function(req,res,next){
+    var target=path+(req.url.slice(1)||def);
+    fs.exists(target,function(e){
+      if(!e)return (next||F404)(req,res);
+      else{
+        readAndDo(target,function(erm){
+          var C=0;
+          var O={};
+          console.log('\n'+target);
+          res.end(erm.replace(/<\?js(.|\s)*?\?>/mg,function(x){
+            var R="";
+            var echo=function(x){
+              R+=(x+'\n');
+              console.log(x);
+              return x;
+            };
+            eval(x.slice(4,-3));
+            return R;  
+          }));
+        });
+      }
+    });
   };
 };
 
@@ -37,5 +47,6 @@ if(!module.parent){
   var server=http.createServer(main({path:'views/'}));
   server.listen(8080);
 }else{
+  console.log("exporting self");
   module.exports=php;
 }
