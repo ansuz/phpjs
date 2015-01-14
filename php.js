dffc63f1e529f460d5d1263581a9d2b019f0a88f
ansuz <ansuz@transitiontech.ca>
2015-01-14 13:27:39 -0500

    handle asynchrony and other stuff

--- php.js
+++ php.js
@@ -1,5 +1,6 @@
 var http=require("http");
 var fs=require("fs");
+var $=require("ansuz");
 var php={};
 
 var readAndDo=php.readAndDo=function(path,f){
@@ -17,6 +18,7 @@ var main=php.main=function(opt){
   var F404=function(req,res){
     res.end('404');
   };
+  var wait=opt.wait||false;
 
   return function(req,res,next){
     var target=path+(req.url.slice(1)||def);
@@ -24,19 +26,35 @@ var main=php.main=function(opt){
       if(!e)return (next||F404)(req,res);
       else{
         readAndDo(target,function(erm){
+          var php={
+            wait:wait
+            ,response:{}
+            ,body:""
+            ,end:function(){
+              res.end($.swap(php.body,php.response));
+            }
+          };
           var C=0;
-          var O={};
-          console.log('\n'+target);
-          res.end(erm.replace(/<\?js(.|\s)*?\?>/mg,function(x){
-            var R="";
+          php.body+=erm.replace(/<\?js(.|\s)*?\?>/mg,function(x){
+            var id=php.id='{PHPJS'+(C++)+'}';
+            php.response[id]="";
             var echo=function(x){
-              R+=(x+'\n');
+              php.response[id]+=(x+'\n');
               console.log(x);
               return x;
             };
-            eval(x.slice(4,-3));
-            return R;  
-          }));
+            try{
+              eval(x.slice(4,-3)); // eval blocks
+              // its processes may not, use php.wait=true
+            }catch(err){
+              console.log('ERR: '+err);
+            }
+            php.response[id];
+            return id;  
+          });
+        if(!php.wait){  
+          res.end($.swap(php.body,php.response));
+        }
         });
       }
     });
